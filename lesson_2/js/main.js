a61562c0d642c3612b0d0ece0b1c84fcd3f7e6b5
IvanPC <ivkuds@gmail.com>
2021-02-22 15:51:08 +0300

    lesson 2 without cart

--- lesson_2/js/main.js
+++ lesson_2/js/main.js
@@ -4,65 +4,34 @@ class Products {
     container = null;
 
     constructor(selector) {
-        this.container = document.querySelector(selector)
+        this.container = document.querySelector(selector);
+        this._fetchData();
+        this._render();
+    }
+    calcSum(){
+        let sum = 0;
+
+        for (let product of this.products){
+            sum+= product.price
+        }
+        return sum;
+
     }
 
-    fetchData() {
+    _fetchData() {
         this.data = [
-            {
-                title: 'Notebook',
-                id: 1,
-                price: 2000
-            },
-            {
-                title: 'Keyboard',
-                id: 2,
-                price: 200
-            },
-            {
-                title: 'Mouse',
-                id: 3,
-                price: 100
-            },
-            {
-                title: 'Gamepad',
-                id: 4,
-                price: 87
-            },
-            {
-                title: 'Screen',
-                id: 5,
-                price: 12300
-            },
-            {
-                title: 'Fan',
-                id: 6,
-                price: 3250
-            },
-            {
-                title: 'Mat',
-                id: 7,
-                price: 1100
-            },
-            {
-                title: 'Chair',
-                id: 8,
-                price: 8600
-            },
-            {
-                title: 'Headphones',
-                id: 9,
-                price: 6780
-            }
-
+            { title: 'Notebook', id: 1, price: 2000 },
+            { title: 'Keyboard', id: 2, price: 200 },
+            { title: 'Mouse', id: 3, price: 100 },
+            { title: 'Gamepad', id: 4, price: 87 }
         ];
     }
 
-    render() {
+    _render() {
         for (let data of this.data) {
             const product = new ProductItem(data);
             this.products.push(product);
-            this.container.insertAbjacentHTML('beforeend', product.render())
+            this.container.insertAdjacentHTML('beforeend', product.render())
         }
     }
 }
@@ -72,25 +41,37 @@ class ProductItem {
     price = 0;
     id = 0;
     img = '';
+    key = '';
 
     constructor(product, img = 'https://placehold.it/200x150') {
-        ({title: this.title, price: this.price, id: this.id} = product);
+        ({ title: this.title, price: this.price, id: this.id } = product);
         this.img = img;
     }
 
     render() {
         return `<div class="product-item">
-    <img scr = "${this.img}" alt="${this.title}">
-    <div class="desc">
-    <h3>${this.title}</h3>
-    <p>${this.price}</p>
-    <button class="by-btn">Купить</button>
-    </div>
-    </div>`;
+                 <img src="${this.img}" alt="${this.title}">
+                 <div class="desc">
+                     <h3>${this.title}</h3>
+                     <p>${this.price}</p>
+                     <button class="by-btn">Купить</button>
+                 </div>
+             </div>`
     }
 }
 
-const list = new Products('.products');
 
-list.fetchData();
-list.render();
+class Cart {
+    // some - cartItems array
+
+    // someMethod() - метод делает то-то
+}
+
+class CartItem {
+    // some - cartItems array
+
+    // someMethod() - метод делает то-то
+}
+
+const list = new Products('.products');
+console.log(list.calcSum());
